af0d6a523399fafb15965f70661c89c7d552ff15
grafiaviles <grafiaviles@gmail.com>
2017-11-20 03:02:45 -0300

    Section services ok

--- www/js/effects.js
+++ www/js/effects.js
@@ -20,3 +20,5 @@ $(document).ready(function() {
         swipeable: true
     });
 });
+// Navegacion Ancla
+$(function() { $(".anclas").click(function() { if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) { var e = $(this.hash); if ((e = e.length && e || $("[name=" + this.hash.slice(1) + "]")).length) { var a = e.offset().top; return $("html,body").animate({ scrollTop: a }, 800), !1 } } }) });
