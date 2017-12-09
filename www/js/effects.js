88711401ccb730583a67ade031ed3b9e16d77c41
grafiaviles <grafiaviles@gmail.com>
2017-12-09 20:24:56 -0300

    Menu editando

--- www/js/effects.js
+++ www/js/effects.js
@@ -6,7 +6,10 @@ $(window).scroll(function() {
         e.preventDefault(),
             $("html, body").animate({ scrollTop: 0 }, 800)
     });
-
+//Menu Principal
+$(".button-collapse").sideNav({
+    menuWidth: 270
+});
 // Acordeon
 $('.collapsible').collapsible();
 // Toolstips
@@ -14,15 +17,34 @@ $('.tooltipped').tooltip('remove');
 // Paralax
 $('.parallax').parallax();
 // Tabs Equipos Home slider
-
 $(document).ready(function() {
     $('ul.tabs').tabs({
         swipeable: true
     });
 });
+//Modal buscador
+$('.modal').modal();
 // Navegacion Ancla
 $(function() { $(".anclas").click(function() { if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) { var e = $(this.hash); if ((e = e.length && e || $("[name=" + this.hash.slice(1) + "]")).length) { var a = e.offset().top; return $("html,body").animate({ scrollTop: a }, 800), !1 } } }) });
 
 //Slider Home
-// Pause slider
 $('.slider').slider();
+
+$('.dropdown-button').dropdown({
+    hover: true, // Activate on hover
+    belowOrigin: true, // Displays dropdown below the button
+});
+
+//Efecto Menu
+var altura = $('.efecto-menu').offset().top;
+
+$(window).on('scroll', function() {
+    if ($(window).scrollTop() > altura) {
+        $('.efecto-menu').addClass('menu-fixed');
+    } else {
+        $('.efecto-menu').removeClass('menu-fixed');
+    }
+});
+
+//Selectores
+$('select').material_select();
